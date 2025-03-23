3c1f635fc5efe279f43bd7effc76c0caa54bc6c0
Claire <macaroonforu@gmail.com>
2025-03-22 20:26:15 -0400

    more organizaing

--- web_ui/__init__.py
+++ web_ui/__init__.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
-
-
-    @app.route("/")
+    @app.route("/all_good")
     def hello_world():
-        return render_template('layout.html', title='Home')
+        return render_template('all_good.html', title='Home')
+    
+    @app.route("/all_bad")
+    def all_bad():
+            return render_template('danger.html', title='Home')
+    
+    
     return app
