3c0a0c5d6b805f65ee07d896def813db2081bbb4
Claire <macaroonforu@gmail.com>
2025-03-22 20:33:47 -0400

    most recent changes

--- web_ui/__init__.py
+++ web_ui/__init__.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template
-
-
 def create_app():
     app = Flask(__name__)
     @app.route("/all_good")
     def hello_world():
         return render_template('all_good.html', title='Home')
-    
     @app.route("/all_bad")
     def all_bad():
             return render_template('danger.html', title='Home')
-    
-    
     return app
